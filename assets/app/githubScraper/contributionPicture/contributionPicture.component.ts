6c829058abcc0d7d511e19bc8634fd6c90ccdc26
Jonathan <poisson.jonathan777@gmail.com>
2016-08-07 18:51:34 -0400

    Cleanup of contribution image type

--- assets/app/githubScraper/contributionPicture/contributionPicture.component.ts
+++ assets/app/githubScraper/contributionPicture/contributionPicture.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
-import { DomSanitizationService } from '@angular/platform-browser';
+import { DomSanitizationService, SafeHtml } from '@angular/platform-browser';
 import 'rxjs/Rx'; // .map()
 import {Observable} from "rxjs/Observable";
 
@@ -11,33 +11,17 @@ import {Observable} from "rxjs/Observable";
   templateUrl: 'contributionPicture.html'
 })
 export class ContributionPicture {
-  html;
+  public html: SafeHtml;
 
   constructor(private _http: Http, private sanitizer: DomSanitizationService) {
     this.getImage();
   }
 
   getImage() {
-    console.log("running");
     this._http.get('http://localhost:3000/githubContribution')
       .subscribe(response => {
-        console.log(response);
-        // var el = document.createElement('html');
-        // el.innerHTML = response.text();
-        // var squares = el.getElementsByTagName("rect");
-        // for (var i = squares.length; i >=0; i--) {
-        //   console.log(squares[i]);
-        // }
-
-        // for (var i = 0; i < allChilds.length; i++) { // body
-        //   for (var j = 0; i < allChilds[i].childNodes.length; i++) { // svg
-        //     console.log(allChilds[i])
-        //   }
-        // }
-
         var incomingHtml = response.json().image;
         this.html = this.sanitizer.bypassSecurityTrustHtml(incomingHtml);
       })
-      // .catch(error => Observable.throw(error.json()));
   }
 }
