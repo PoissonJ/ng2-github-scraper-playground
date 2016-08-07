eb9f184a54ffe9923a5e79d25676f5e79dea0db0
Jonathan <poisson.jonathan777@gmail.com>
2016-08-06 20:40:23 -0400

    Added folder and setup for getting the contribution image

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ContributionPicture } from './githubScraper/contributionPicture/index';
 
 @Component({
   moduleId: module.id,
   selector: 'my-app',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  directives:[ContributionPicture]
 })
 export class AppComponent {
 
---
+++ assets/app/githubScraper/contributionPicture/index.ts
@@ -0,0 +1,1 @@
+export * from './contributionPicture.component';
