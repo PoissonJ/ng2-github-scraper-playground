8e8209563dd30d622616f099ea21f0d76a9d02a6
Jonathan <poisson.jonathan777@gmail.com>
2016-08-07 18:43:36 -0400

    Got contribution picture to show

--- assets/app/githubScraper/contributionPicture/contributionPicture.component.ts
+++ assets/app/githubScraper/contributionPicture/contributionPicture.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
+import { DomSanitizationService } from '@angular/platform-browser';
 import 'rxjs/Rx'; // .map()
 import {Observable} from "rxjs/Observable";
+
 
 @Component({
   moduleId: module.id,
@@ -11,21 +13,21 @@ import {Observable} from "rxjs/Observable";
 export class ContributionPicture {
   html;
 
-  constructor(private _http: Http) {
+  constructor(private _http: Http, private sanitizer: DomSanitizationService) {
     this.getImage();
   }
 
   getImage() {
     console.log("running");
-    this._http.get('https://github.com/users/poissonj/contributions')
+    this._http.get('http://localhost:3000/githubContribution')
       .subscribe(response => {
         console.log(response);
-        var el = document.createElement('html');
-        el.innerHTML = response.text();
-        var squares = el.getElementsByTagName("rect");
-        for (var i = squares.length; i >=0; i--) {
-          console.log(squares[i]);
-        }
+        // var el = document.createElement('html');
+        // el.innerHTML = response.text();
+        // var squares = el.getElementsByTagName("rect");
+        // for (var i = squares.length; i >=0; i--) {
+        //   console.log(squares[i]);
+        // }
 
         // for (var i = 0; i < allChilds.length; i++) { // body
         //   for (var j = 0; i < allChilds[i].childNodes.length; i++) { // svg
@@ -33,7 +35,8 @@ export class ContributionPicture {
         //   }
         // }
 
-        this.html = 'done';
+        var incomingHtml = response.json().image;
+        this.html = this.sanitizer.bypassSecurityTrustHtml(incomingHtml);
       })
       // .catch(error => Observable.throw(error.json()));
   }
--- routes/app.js
+++ routes/app.js
@@ -1,9 +1,34 @@
 var express = require('express');
+var cheerio = require('cheerio');
+var request = require('request');
+var gs = require('github-scraper');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  // res.send(url);
   res.render('index');
+});
+
+router.get('/githubContribution', function(req, res, next) {
+  url = "https://github.com/users/poissonj/contributions";
+
+  request(url, function(error, response, html) {
+
+    if (!error) {
+      var $ = cheerio.load(html)
+      console.log(html);
+      res.status(200).json({
+        image: html
+      });
+      // res.send(html);
+
+    } else {
+      res.send(error)
+  }
+
+  })
 });
 
 module.exports = router;
